1f65019d812a81acded9eb80323d5706d2758e04
devonhg <devon.hugh.godfrey@gmail.com>
2015-06-15 01:12:47 -0400

    modified header and footer

--- footer.php
+++ footer.php
@@ -11,11 +11,18 @@
 
 	</div><!-- #content -->
 
+	<?php if ( is_active_sidebar( 'lower-sidebar' ) ) { ?>
+		<div class="sidebar-lower" role="complementary">
+			<?php dynamic_sidebar( 'lower-sidebar' ); ?>
+		</div><!-- #primary-sidebar -->
+	<?php } ?>
+	
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
+			<?php /*
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', '_s' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', '_s' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			<?php  printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); */?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
